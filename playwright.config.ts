ff65e2c1ab1c31c121ef72675ca2c0e5680d7bbf
velikinti <166692454+velikinti@users.noreply.github.com>
2026-05-07 11:58:26 +0530

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 60_000,
+  expect: {
+    timeout: 15_000,
+  },
+  use: {
+    baseURL: 'https://www.epam.com',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
