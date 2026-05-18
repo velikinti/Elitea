05f3e7e09fb1ef7d8e5842ae723cf1a1fc962ba6
velikinti <166692454+velikinti@users.noreply.github.com>
2026-05-18 10:50:12 +0530

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 60_000,
+  expect: { timeout: 10_000 },
+  use: {
+    baseURL: 'https://demowebshop.tricentis.com',
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
