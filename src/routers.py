1a2e9fc664737d0ad01f6901edf43948d45fa4c9
velikinti <166692454+velikinti@users.noreply.github.com>
2026-08-07 10:14:51 +0530

    Mount jobs router in main API router

--- src/routers.py
+++ src/routers.py
@@ -1,5 +1,7 @@
 from fastapi.routing import APIRouter
-from endpoints.automation.routes  import router as automation_router
+from endpoints.automation.routes import router as automation_router
+from endpoints.jobs.routes import router as jobs_router
 
 api_router = APIRouter()
-api_router.include_router(automation_router, tags=["automation"])   
+api_router.include_router(automation_router, tags=["automation"])
+api_router.include_router(jobs_router, tags=["jobs"])
