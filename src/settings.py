d6e4212c49b270bed8b9ba04266594a55842a6a8
velikinti <166692454+velikinti@users.noreply.github.com>
2026-08-07 11:35:06 +0530

    Add QA_API_KEY setting

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,5 @@
 import ast
-from typing import List,Literal,Union,Optional
+from typing import List, Literal, Union, Optional
 from pydantic import validator
 from pydantic_settings import BaseSettings
 
@@ -7,13 +7,15 @@ from pydantic_settings import BaseSettings
 from logconfig import LogSettings
 from dotenv import load_dotenv
 
-Environments=Literal["dev","prod","test","demo"]
-Loglevels=Literal["DEBUG","INFO","WARNING","ERROR","NOTSET"]
+Environments = Literal["dev", "prod", "test", "demo"]
+Loglevels = Literal["DEBUG", "INFO", "WARNING", "ERROR", "NOTSET"]
 
 load_dotenv()
 
+
 class Settings(BaseSettings):
     """Application settings for FastAPI.These settings are loaded from environment variables or .env file."""
+
     environment: Environments = "dev"
 
     host: str
@@ -24,16 +26,19 @@ class Settings(BaseSettings):
     # Enable uviorn loading
     reload: bool = False
 
-    #List of allowed hosts for CORS middleware
+    # List of allowed hosts for CORS middleware
     cors_origins: List[str]
 
-    #Open API settings
-    openapi_key:str
-    openapi_base:str
-    openapi_type:str
-    openapi_version:str
+    # Open API settings
+    openapi_key: str
+    openapi_base: str
+    openapi_type: str
+    openapi_version: str
 
-    #logging settings
+    # Optional API key
+    api_key: Optional[str] = None
+
+    # logging settings
     log: LogSettings
 
     # Test framework configuration
@@ -47,24 +52,25 @@ class Settings(BaseSettings):
             try:
                 result = ast.literal_eval(v)
                 if isinstance(result, list) and all(isinstance(i, str) for i in result):
-                    return [i.strip('/') for i in result]
+                    return [i.strip("/") for i in result]
                 else:
                     raise ValueError("Invalid list format")
-            except(SyntaxError, ValueError):
-                return [i.strip().rstrip('/') for i in v.split(",")]
+            except (SyntaxError, ValueError):
+                return [i.strip().rstrip("/") for i in v.split(",")]
         elif isinstance(v, list) and all(isinstance(i, str) for i in v):
-            return [i.rstrip('/') for i in v]
+            return [i.rstrip("/") for i in v]
         raise ValueError("Invalid CORS origins format")
 
     @property
     def is_production(self):
-        return  True if self.environment == "prod" else False
+        return True if self.environment == "prod" else False
 
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
         env_prefix = "QA_"
         str_strip_whitespace = True
-        env_nested_delimiter = '__'
-        
+        env_nested_delimiter = "__"
+
+
 settings = Settings()
